59dc93f2a4fefadfd212e362887d4f3201ffb3b5
Kailash Anantha <kailash@polymer.fyi>
2023-06-07 18:55:49 +0530

    Adding file upload support

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,11 @@ import React, { useEffect, useState } from 'react';
 // Rest of the imports
 
 import * as Dialog from '@radix-ui/react-dialog';
-import { HiCheckCircle, HiPlus, HiX } from 'react-icons/hi';
+import { HiCheckCircle, HiPlus, HiTrash, HiX, HiXCircle } from 'react-icons/hi';
 import { BsGoogle, BsDiscord, BsCloudUpload } from 'react-icons/bs';
-import { RxNotionLogo } from 'react-icons/rx';
-import { SiSlack } from 'react-icons/si';
+import { FileUploader } from 'react-drag-drop-files';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
 
 import {
@@ -15,6 +16,8 @@ import {
   LinkIcon,
   LockClosedIcon,
 } from '@heroicons/react/20/solid';
+
+const fileTypes = ['txt', 'csv'];
 
 const CarbonAnnouncement = ({ setActiveStep }) => {
   return (
@@ -89,14 +92,14 @@ const ThirdPartyList = ({
   activeIntegrations,
 }) => {
   const integrationsList = [
-    {
-      id: 'notion',
-      subpath: 'notion',
-      name: 'Notion',
-      icon: <RxNotionLogo className="w-8 h-8" />,
-      description: 'Lets your users connect their Notion accounts to Carbon.',
-      active: true,
-    },
+    // {
+    //   id: 'notion',
+    //   subpath: 'notion',
+    //   name: 'Notion',
+    //   icon: <RxNotionLogo className="w-8 h-8" />,
+    //   description: 'Lets your users connect their Notion accounts to Carbon.',
+    //   active: true,
+    // },
     {
       active: true,
       name: 'Google Docs',
@@ -107,47 +110,48 @@ const ThirdPartyList = ({
       icon: <BsGoogle className="w-7 h-7" />,
       data_source_type: 'GOOGLE_DOCS',
     },
+    // {
+    //   active: true,
+    //   name: 'Google Drive',
+    //   subpath: 'google',
+    //   id: 'googleDrive',
+    //   description: 'Lets your users connect their Google Docs to Carbon.',
+    //   scope: 'drive',
+    //   icon: <BsGoogle className="w-7 h-7" />,
+    // },
+    // {
+    //   active: true,
+    //   name: 'Gmail',
+    //   subpath: 'google',
+    //   id: 'gmail',
+    //   description: 'Lets your users connect their Google Docs to Carbon.',
+    //   scope: 'gmail',
+    //   icon: <BsGoogle className="w-7 h-7" />,
+    // },
+    // {
+    //   active: false,
+    //   name: 'Slack',
+    //   subpath: 'slack',
+    //   id: 'slack',
+    //   description: 'Lets your users connect their Slack accounts to Carbon.',
+    //   icon: <SiSlack className="w-7 h-7" />,
+    // },
+    // {
+    //   active: false,
+    //   name: 'Discord',
+    //   subpath: 'discord',
+    //   id: 'discord',
+    //   description: 'Lets your users connect their Discord accounts to Carbon.',
+    //   icon: <BsDiscord className="w-7 h-7" />,
+    // },
     {
       active: true,
-      name: 'Google Drive',
-      subpath: 'google',
-      id: 'googleDrive',
-      description: 'Lets your users connect their Google Docs to Carbon.',
-      scope: 'drive',
-      icon: <BsGoogle className="w-7 h-7" />,
-    },
-    {
-      active: true,
-      name: 'Gmail',
-      subpath: 'google',
-      id: 'gmail',
-      description: 'Lets your users connect their Google Docs to Carbon.',
-      scope: 'gmail',
-      icon: <BsGoogle className="w-7 h-7" />,
-    },
-    {
-      active: false,
-      name: 'Slack',
-      subpath: 'slack',
-      id: 'slack',
-      description: 'Lets your users connect their Slack accounts to Carbon.',
-      icon: <SiSlack className="w-7 h-7" />,
-    },
-    {
-      active: false,
-      name: 'Discord',
-      subpath: 'discord',
-      id: 'discord',
-      description: 'Lets your users connect their Discord accounts to Carbon.',
-      icon: <BsDiscord className="w-7 h-7" />,
-    },
-    {
-      active: false,
       name: 'File Upload',
       subpath: 'local',
       id: 'localFiles',
       description: 'Lets your users upload local files to Carbon.',
       icon: <BsCloudUpload className="w-7 h-7" />,
+      data_source_type: 'LOCAL_FILE',
     },
   ];
 
@@ -159,6 +163,11 @@ const ThirdPartyList = ({
           id: userid,
           apikey: apikey,
           scope: service.scope,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${apikey}`,
+          'customer-id': userid,
         },
       }
     );
@@ -207,9 +216,14 @@ const ThirdPartyList = ({
                 }`}
                 onClick={() => {
                   if (integration.active) {
+                    if (integration.data_source_type === 'LOCAL_FILE') {
+                      setActiveStep(integration.data_source_type);
+                      return;
+                    }
                     if (integrationStatus) {
-                      console.log('Integration already active');
-                      setActiveStep('GOOGLE_DOCS');
+                      // handleServiceOAuthFlow(integration);
+                      // console.log('Integration already active');
+                      setActiveStep(integration.data_source_type);
                     } else {
                       handleServiceOAuthFlow(integration);
                     }
@@ -249,6 +263,7 @@ const GoogleDocsSelector = ({
   userid,
 }) => {
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [syncResponse, setSyncResponse] = useState(null);
 
   const syncSelectedFiles = async () => {
     const syncResponse = await axios.post(
@@ -261,8 +276,13 @@ const GoogleDocsSelector = ({
     );
 
     if (syncResponse.status === 200 && syncResponse.data) {
-      console.log('Sync Response: ', syncResponse.data);
-      setActiveStep(2);
+      setSyncResponse(syncResponse.data);
+      // console.log('Sync Response: ', syncResponse.data);
+      // if (syncResponse.data.status === 200) {
+      //   setActiveStep(2);
+
+      // }
+      // setActiveStep(2);
     }
   };
 
@@ -277,43 +297,179 @@ const GoogleDocsSelector = ({
           <h1>Select Files</h1>
         </div>
       </Dialog.Title>
-      <div className="flex flex-col space-y-3 w-full py-2 overflow-y-auto">
-        {integrationData.token.all_files.map((fileData) => {
-          const isSelected = selectedFiles.includes(fileData.id);
+      {!syncResponse && (
+        <>
+          <div className="flex flex-col space-y-3 w-full py-2 overflow-y-auto">
+            {integrationData.token.all_files.map((fileData) => {
+              const isSelected = selectedFiles.includes(fileData.id);
 
-          return (
-            <div
-              key={fileData.id}
-              className={`border rounded-md h-fit items-center p-4 w-full cursor-pointer ${
-                isSelected ? 'bg-green-200' : 'bg-white hover:bg-gray-100'
-              }`}
-              onClick={() => {
-                setSelectedFiles((prev) => {
-                  if (prev.includes(fileData.id)) {
-                    return prev.filter((id) => id !== fileData.id);
-                  } else {
-                    return [...prev, fileData.id];
-                  }
-                });
-              }}
+              return (
+                <div
+                  key={fileData.id}
+                  className={`border rounded-md h-fit items-center p-4 w-full cursor-pointer ${
+                    isSelected ? 'bg-green-200' : 'bg-white hover:bg-gray-100'
+                  }`}
+                  onClick={() => {
+                    setSelectedFiles((prev) => {
+                      if (prev.includes(fileData.id)) {
+                        return prev.filter((id) => id !== fileData.id);
+                      } else {
+                        return [...prev, fileData.id];
+                      }
+                    });
+                  }}
+                >
+                  <h1 className="text-md font-normal">{fileData.name}</h1>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="flex flex-col h-full space-y-2 w-full">
+            <button
+              className="w-full h-12 flex flex-row bg-black text-white items-center justify-center rounded-md cursor-pointer"
+              onClick={() => syncSelectedFiles()}
             >
-              <h1 className="text-md font-normal">{fileData.name}</h1>
-            </div>
-          );
-        })}
-      </div>
+              Sync Files
+            </button>
+          </div>
+        </>
+      )}
 
-      <div className="flex flex-col h-full space-y-2 w-full">
-        <button
-          className="w-full h-12 flex flex-row bg-black text-white items-center justify-center rounded-md cursor-pointer"
-          onClick={() => syncSelectedFiles()}
-        >
-          Sync Files
-        </button>
-      </div>
+      {syncResponse && (
+        <div className="flex flex-col space-y-3 w-full py-2 overflow-y-auto h-full items-center text-xl justify-center">
+          {syncResponse.code === 200 ? (
+            <HiCheckCircle className="text-green-500 w-8 h-8" />
+          ) : (
+            <HiXCircle className="text-red-500  w-8 h-8" />
+          )}
+          <p>{syncResponse.message}</p>
+        </div>
+      )}
     </div>
   );
 };
+
+function FileUpload({ setActiveStep, apikey, userid }) {
+  const [file, setFile] = useState(null);
+  const [syncResponse, setSyncResponse] = useState(null);
+
+  useEffect(() => {
+    console.log('Files: ', file);
+  }, [file]);
+
+  const uploadSelectedFile = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const uploadResponse = await axios.post(
+        `https://api.dev.carbon.ai/uploadfile`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${apikey}`,
+            'customer-id': userid,
+          },
+        }
+      );
+
+      if (uploadResponse.status === 200 && uploadResponse.data) {
+        setSyncResponse(uploadResponse.data);
+        // console.log('Sync Response: ', syncResponse.data);
+        // if (syncResponse.data.status === 200) {
+        //   setActiveStep(2);
+
+        // }
+        // setActiveStep(2);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error('Error uploading file');
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-[540px] items-center relative">
+      <Dialog.Title className="text-lg mb-4 font-medium w-full">
+        <div className="w-full flex items-center space-x-4">
+          <ArrowLeftIcon
+            onClick={() => setActiveStep(1)}
+            className="cursor-pointer h-6 w-6 text-gray-400"
+          />
+          <h1>Upload Files</h1>
+        </div>
+      </Dialog.Title>
+      {!syncResponse && (
+        <div className="w-full h-full flex-col flex space-y-4 justify-between">
+          <FileUploader
+            multiple={false}
+            handleChange={setFile}
+            name="file"
+            types={fileTypes}
+            maxSize="9.5"
+            label="Upload or drag a file here to embed."
+          >
+            <div className="flex py-2 h-40 w-full flex mt-4 mb-1 cursor-pointer text-center border rounded-lg justify-center items-center gap-x-2  overflow-hidden text-black space-x-2">
+              <div>
+                <AiOutlineCloudUpload className="w-6 h-6 mb-4 mx-auto" />
+                <p className="text-[#484848]">
+                  Upload .txt, .pdf, or .csv files
+                </p>
+                <p className="text-[#919191]">
+                  Max 3 MB per File (20 MB total)
+                </p>
+              </div>
+            </div>
+          </FileUploader>
+
+          {file && (
+            <table class="my-3 w-full bg-blue-400/20">
+              <tr>
+                <td class="py-2 px-4">Name:</td>
+                <td class="py-2 px-4 break-words text-left">{file.name}</td>
+              </tr>
+              <tr>
+                <td class="py-2 px-4">Size:</td>
+                <td class="py-2 px-4 text-left">{`${parseFloat(
+                  file.size / 1024
+                ).toFixed(2)} KB`}</td>
+              </tr>
+              <tr>
+                <td class="py-2 px-4">File Type:</td>
+                <td class="py-2 px-4 text-left">{file.type}</td>
+              </tr>
+            </table>
+          )}
+
+          <div className="flex flex-col h-full space-y-2 w-full">
+            <button
+              className="w-full h-12 flex flex-row bg-black text-white items-center justify-center rounded-md cursor-pointer"
+              onClick={() => {
+                if (file) uploadSelectedFile();
+                else toast.error('Please select a file to upload');
+              }}
+            >
+              Upload File
+            </button>
+          </div>
+        </div>
+      )}
+
+      {syncResponse && (
+        <div className="flex flex-col space-y-3 w-full py-2 overflow-y-auto h-full items-center text-xl justify-center">
+          {syncResponse.code === 200 ? (
+            <HiCheckCircle className="text-green-500 w-8 h-8" />
+          ) : (
+            <HiXCircle className="text-red-500  w-8 h-8" />
+          )}
+          <p>{syncResponse.message}</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 const CarbonConnect = ({ apikey, userid }) => {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -386,7 +542,20 @@ const CarbonConnect = ({ apikey, userid }) => {
               setActiveStep={setActiveStep}
             />
           )}
+          {activeStep === 'LOCAL_FILE' && (
+            <FileUpload
+              apikey={apikey}
+              userid={userid}
+              setActiveStep={setActiveStep}
+            />
+          )}
         </Dialog.Content>
+
+        <ToastContainer
+          position="bottom-right"
+          pauseOnFocusLoss={false}
+          pauseOnHover={false}
+        />
       </Dialog.Portal>
     </Dialog.Root>
   );
