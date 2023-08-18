d1e2258c8e4e46815fbd160fcb77d0a6493ba800
Kailash Anantha <kailash@polymer.fyi>
2023-08-18 22:16:36 +0530

    Adding support for sitemaps in CC

--- src/components/WebScraper.js
+++ src/components/WebScraper.js
@@ -9,6 +9,8 @@ import {
   HiX,
   HiPlus,
   HiTrash,
+  HiDownload,
+  HiInformationCircle,
 } from 'react-icons/hi';
 
 import { toast } from 'react-toastify';
@@ -17,6 +19,7 @@ import '../index.css';
 import { BASE_URL } from '../constants';
 import { LuLoader2 } from 'react-icons/lu';
 import { useCarbon } from '../contexts/CarbonContext';
+import { BiLoaderAlt } from 'react-icons/bi';
 
 const DEFAULT_RECURSION_DEPTH = 3;
 const DEFAULT_MAX_PAGES_TO_SCRAPE = 100;
@@ -34,9 +37,32 @@ function WebScraper({
   secondaryTextColor,
 }) {
   const MAX_URLS = 2;
-  const [urls, setUrls] = useState(['']);
+
+  // These will be used to store the list of URLs to be scraped.
+  const [urls, setUrls] = useState(['']); // List of URLs to be scraped.
+  const [sitemapUrls, setSitemapUrls] = useState([]); // List of URLs to be scraped selected from the sitemap.
+
+  const [areUrlsProvided, setAreUrlsProvided] = useState(false); // Flag to check if the user has provided any URLs to be scraped.
+  const [areSitemapUrlsProvided, setAreSitemapUrlsProvided] = useState(false); // Flag to check if the user has provided any URLs to be scraped from the sitemap.
+
   const [scrapingResponse, setScrapingResponse] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState('webpages');
+  const [sitemapUrl, setSitemapUrl] = useState('');
+
+  const [sitemapUrlsLoading, setSitemapUrlsLoading] = useState(false);
+  const [sitemapUrlsError, setSitemapUrlsError] = useState(null);
+  const [selectedUrlIds, setSelectedUrlIds] = useState([]);
+  const [selectAllUrls, setSelectAllUrls] = useState(false);
+  const [service, setService] = useState(null);
+
+  useEffect(() => {
+    setService(
+      processedIntegrations.find(
+        (integration) => integration.id === 'WEB_SCRAPER'
+      )
+    );
+  }, [processedIntegrations]);
 
   const {
     accessToken,
@@ -55,9 +81,9 @@ function WebScraper({
         return;
       }
 
-      const service = processedIntegrations.find(
-        (integration) => integration.id === 'WEB_SCRAPER'
-      );
+      // const service = processedIntegrations.find(
+      //   (integration) => integration.id === 'WEB_SCRAPER'
+      // );
       const chunkSize =
         service?.chunkSize || topLevelChunkSize || defaultChunkSize;
       const overlapSize =
@@ -77,7 +103,14 @@ function WebScraper({
         'i'
       ); // fragment locator
 
-      let validUrls = urls.filter((url) => urlPattern.test(url));
+      const selectedSitemapUrls = sitemapUrls.filter((url, idx) =>
+        selectedUrlIds.includes(idx)
+      );
+
+      let validUrls =
+        activeTab === 'sitemap'
+          ? selectedSitemapUrls
+          : urls.filter((url) => urlPattern.test(url));
 
       if (validUrls.length === 0) {
         toast.error('Please provide at least one valid URL.');
@@ -85,20 +118,26 @@ function WebScraper({
         return;
       }
 
-      const requestObject = validUrls.map((url) => ({
-        url: url,
-        tags: tags,
-        recursion_depth: recursionDepth,
-        max_pages_to_scrape: maxPagesToScrape,
-        // repeat: false,
-        // repeat_interval: 0,
-        chunk_size: chunkSize,
-        chunk_overlap: overlapSize,
-      }));
+      const requestObject =
+        activeTab === 'sitemap'
+          ? selectedSitemapUrls.map((url) => ({
+              url: url,
+              tags: tags,
+              recursion_depth: 1,
+              max_pages_to_scrape: 1,
+              chunk_size: chunkSize,
+              chunk_overlap: overlapSize,
+            }))
+          : validUrls.map((url) => ({
+              url: url,
+              tags: tags,
+              recursion_depth: recursionDepth,
+              max_pages_to_scrape: maxPagesToScrape,
+              chunk_size: chunkSize,
+              chunk_overlap: overlapSize,
+            }));
 
       const uploadResponse = await authenticatedFetch(
-        // `${BASE_URL[environment]}/web_scrape`,
-        // `${BASE_URL[environment]}/integrations/web_scrape`,
         `${BASE_URL[environment]}/web_scrape`,
         {
           method: 'POST',
@@ -107,19 +146,10 @@ function WebScraper({
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(requestObject),
-          // body: JSON.stringify({
-          //   urls: validUrls,
-          //   tags: tags,
-          //   repeat: false,
-          //   repeat_interval: 0,
-          //   chunk_size: chunkSize,
-          //   chunk_overlap: overlapSize,
-          // }),
         }
       );
 
       if (uploadResponse.status === 200) {
-        // const uploadResponseData = await uploadResponse.json();
         setScrapingResponse('Scraping request initiated successfully.');
         onSuccess({
           status: 200,
@@ -153,6 +183,41 @@ function WebScraper({
     });
   };
 
+  const handleSitemapUrlChange = (url) => {
+    setSitemapUrl(url);
+  };
+
+  const handleFetchSitemapUrls = async () => {
+    try {
+      if (!sitemapUrl) {
+        toast.error('Please provide a valid sitemap URL.');
+        return;
+      }
+      setSitemapUrlsLoading(true);
+      const response = await authenticatedFetch(
+        `${BASE_URL[environment]}/process_sitemap?url=${sitemapUrl}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Token ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const responseData = await response.json();
+        setSitemapUrls(responseData.urls);
+        setSitemapUrlsLoading(false);
+      } else {
+        throw new Error('Error fetching sitemap. Please try again.');
+      }
+    } catch (error) {
+      toast.error('Error fetching sitemap. Please try again.');
+      setSitemapUrlsError('Error fetching sitemap. Please try again.');
+    }
+  };
+
   const handleRemoveUrl = (url_index) => {
     setUrls((prevList) => {
       let newUrls = [...prevList];
@@ -174,45 +239,216 @@ function WebScraper({
           <h1>Web Scraper</h1>
         </div>
       </Dialog.Title>
+
       {!scrapingResponse && (
-        <div className="cc-w-full cc-h-full cc-flex-col cc-flex cc-space-y-4 cc-justify-between">
-          <div className="cc-flex cc-flex-col cc-justify-start cc-h-full cc-items-start cc-w-full cc-space-y-4">
-            {urls.map((url, idx) => (
-              <div
-                key={idx}
-                className="cc-flex cc-space-x-2 cc-items-center cc-w-full cc-h-10"
-              >
-                <input
-                  type="text"
-                  className="cc-p-2 cc-flex-grow cc-text-gray-700 cc-text-sm cc-border-4 cc-border-gray-400"
-                  style={{ borderRadius: '0.375rem' }}
-                  placeholder="Enter URL"
-                  value={url}
-                  onChange={(e) => handleUrlChange(idx, e.target.value)}
-                />
-                <HiX
-                  className=" cc-text-red-400 cc-text-sm cc-border cc-border-gray-400 cc-w-10 cc-h-10 cc-p-2 cc-cursor-pointer hover:cc-bg-gray-200 hover:cc-border-0"
-                  style={{ borderRadius: '0.375rem' }}
-                  onClick={() => handleRemoveUrl(idx)}
-                />
+        <>
+          <div className="cc-flex cc-w-full">
+            <div
+              onClick={() => setActiveTab('webpages')}
+              className={`cc-py-2 cc-px-4 cc-w-1/2 cc-rounded-t-md cc-text-center cc-cursor-pointer ${
+                activeTab === 'webpages'
+                  ? 'cc-border-b-2 cc-border-blue-500'
+                  : 'cc-border-b cc-border-black'
+              }`}
+            >
+              <p>Websites</p>
+            </div>
+            <div
+              onClick={() => setActiveTab('sitemap')}
+              className={`cc-py-2 cc-px-4 cc-w-1/2 cc-rounded-t-md cc-text-center cc-cursor-pointer ${
+                activeTab === 'sitemap'
+                  ? 'cc-border-b-2 cc-border-blue-500'
+                  : 'cc-border-b cc-border-black'
+              }`}
+            >
+              Sitemap
+            </div>
+          </div>
+          <div className="py-4 cc-flex cc-grow cc-w-full">
+            {activeTab === 'webpages' && (
+              <div className="cc-flex cc-flex-col cc-justify-start cc-h-full cc-items-start cc-w-full cc-space-y-4">
+                {urls.map((url, idx) => (
+                  <div
+                    key={idx}
+                    className="cc-flex cc-space-x-2 cc-items-center cc-w-full cc-h-10"
+                  >
+                    <input
+                      type="text"
+                      className="cc-p-2 cc-flex-grow cc-text-gray-700 cc-text-sm cc-border-4 cc-border-gray-400"
+                      style={{ borderRadius: '0.375rem' }}
+                      placeholder="Enter URL"
+                      value={url}
+                      onChange={(e) => handleUrlChange(idx, e.target.value)}
+                    />
+                    <HiX
+                      className=" cc-text-red-400 cc-text-sm cc-border cc-border-gray-400 cc-w-10 cc-h-10 cc-p-2 cc-cursor-pointer hover:cc-bg-gray-200 hover:cc-border-0"
+                      style={{ borderRadius: '0.375rem' }}
+                      onClick={() => handleRemoveUrl(idx)}
+                    />
+                  </div>
+                ))}
+                {urls.length < MAX_URLS && (
+                  <button
+                    className={`cc-w-fit cc-h-10 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2 cc-px-4`}
+                    style={{
+                      backgroundColor: secondaryBackgroundColor,
+                      color: secondaryTextColor,
+                    }}
+                    onClick={handleAddUrl}
+                  >
+                    <HiPlus className="inline-block mr-2" />
+                    Add
+                  </button>
+                )}
               </div>
-            ))}
-            {urls.length < MAX_URLS && (
-              <button
-                className={`cc-w-fit cc-h-10 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2 cc-px-4`}
-                style={{
-                  backgroundColor: secondaryBackgroundColor,
-                  color: secondaryTextColor,
-                }}
-                onClick={handleAddUrl}
-              >
-                <HiPlus className="inline-block mr-2" />
-                Add
-              </button>
+            )}
+            {activeTab === 'sitemap' && (
+              <div className="cc-flex cc-flex-col cc-justify-start cc-h-full cc-items-start cc-w-full cc-space-y-4">
+                <div className="cc-flex cc-space-x-2 cc-items-center cc-w-full cc-h-10">
+                  <input
+                    type="text"
+                    className="cc-p-2 cc-flex-grow cc-text-gray-700 cc-text-sm cc-border-4 cc-border-gray-400"
+                    style={{ borderRadius: '0.375rem' }}
+                    placeholder="Enter Sitemap URL"
+                    value={sitemapUrl}
+                    onChange={(e) => handleSitemapUrlChange(e.target.value)}
+                  />
+                  <div
+                    className="cc-text-white cc-bg-blue-500 cc-text-sm cc-border cc-border-gray-400 cc-w-14 cc-h-10 cc-p-2 cc-cursor-pointer hover:cc-bg-blue-600 cc-items-center"
+                    style={{ borderRadius: '0.375rem' }}
+                    onClick={handleFetchSitemapUrls}
+                  >
+                    Fetch
+                  </div>
+                </div>
+
+                <div className="cc-w-full cc-h-70 cc-overflow-y-auto">
+                  {sitemapUrlsLoading && (
+                    <div className="cc-h-full cc-w-full cc-items-center cc-justify-center cc-flex">
+                      <BiLoaderAlt className="cc-animate-spin cc-text-5xl" />
+                    </div>
+                  )}
+                  {sitemapUrlsError && (
+                    <div className="cc-h-full cc-w-full cc-items-center cc-justify-center cc-flex">
+                      <HiXCircle className="cc-text-red-500 cc-w-8 cc-h-8" />
+                      <p className="cc-text-center">{sitemapUrlsError}</p>
+
+                      <button
+                        className={`cc-w-fit cc-h-10 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2 cc-px-4`}
+                        style={{
+                          backgroundColor: secondaryBackgroundColor,
+                          color: secondaryTextColor,
+                        }}
+                        onClick={handleFetchSitemapUrls}
+                      >
+                        <HiPlus className="inline-block mr-2" />
+                        Retry
+                      </button>
+
+                      <button
+                        className={`cc-w-fit cc-h-10 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2 cc-px-4`}
+                        style={{
+                          backgroundColor: secondaryBackgroundColor,
+                          color: secondaryTextColor,
+                        }}
+                        onClick={() => setSitemapUrlsError(null)}
+                      >
+                        <HiPlus className="inline-block mr-2" />
+                        Cancel
+                      </button>
+                    </div>
+                  )}
+                  {sitemapUrls.length > 0 && (
+                    <>
+                      {/* // Header for the list of URLs scraped from the sitemap. */}
+                      <div className="cc-flex cc-flex-row cc-items-center cc-w-full cc-h-10 cc-p-2 cc-bg-gray-200 cc-space-x-2">
+                        <input
+                          type="checkbox"
+                          onChange={(e) => {
+                            // const service = processedIntegrations.find(
+                            //   (integration) => integration.id === 'WEB_SCRAPER'
+                            // );
+                            const maxPagesToScrape =
+                              service?.maxPagesToScrape ||
+                              DEFAULT_MAX_PAGES_TO_SCRAPE;
+                            if (e.target.checked) {
+                              if (sitemapUrls.length > maxPagesToScrape) {
+                                toast.error(
+                                  `You can select a maximum of ${maxPagesToScrape} URLs.`
+                                );
+                                return;
+                              }
+                              setSelectAllUrls(true);
+                              setSelectedUrlIds(
+                                sitemapUrls.map((url, idx) => idx)
+                              );
+                            } else {
+                              setSelectAllUrls(false);
+                              setSelectedUrlIds([]);
+                            }
+                          }}
+                          checked={selectAllUrls}
+                        />
+                        <p className="cc-text-sm cc-text-gray-700 cc-font-bold cc-w-full">
+                          URLs
+                        </p>
+                      </div>
+                      {/* // List of URLs displays here along with a checkbox for
+                      each URL. Selecting a URL will add it to the list of URLs 
+                      to be scraped. */}
+                      <div className="cc-flex cc-flex-col cc-justify-start cc-h-64 cc-items-start cc-w-full cc-space-y-4">
+                        {sitemapUrls.map((url, idx) => (
+                          <div
+                            key={idx}
+                            className="cc-flex cc-space-x-2 cc-items-center cc-w-full cc-h-10 cc-p-2"
+                          >
+                            <input
+                              type="checkbox"
+                              onChange={(e) => {
+                                if (e.target.checked) {
+                                  setSelectedUrlIds((prevList) => [
+                                    ...prevList,
+                                    idx,
+                                  ]);
+                                } else {
+                                  setSelectedUrlIds((prevList) => {
+                                    let newIdsList = [...prevList];
+                                    newIdsList.splice(
+                                      newIdsList.indexOf(idx),
+                                      1
+                                    );
+                                    setSelectAllUrls(false);
+                                    return newIdsList;
+                                  });
+                                }
+                              }}
+                              checked={selectedUrlIds.includes(idx)}
+                            />
+                            <p className="cc-text-sm cc-text-gray-700">{url}</p>
+                          </div>
+                        ))}
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
             )}
           </div>
+
+          <p className="cc-flex cc-text-gray-500 cc-pb-2">
+            <HiInformationCircle className="cc-w-4 cc-h-4" />
+            {activeTab === 'sitemap' ? (
+              <span className="text-xs">{`You can select upto ${
+                service?.maxPagesToScrape || DEFAULT_MAX_PAGES_TO_SCRAPE
+              } pages for scraping.`}</span>
+            ) : (
+              <span className="text-xs">{`Scrapes upto a maximum of ${
+                service?.maxPagesToScrape || DEFAULT_MAX_PAGES_TO_SCRAPE
+              } pages per website`}</span>
+            )}
+          </p>
           <button
-            className={`cc-w-full cc-h-12 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2`}
+            className={`cc-w-full cc-h-10 cc-flex cc-flex-row cc-items-center cc-justify-center cc-rounded-md cc-cursor-pointer cc-space-x-2`}
             style={{
               backgroundColor: primaryBackgroundColor,
               color: primaryTextColor,
@@ -226,7 +462,7 @@ function WebScraper({
             )}
             <p>Submit</p>
           </button>
-        </div>
+        </>
       )}
 
       {scrapingResponse && (
